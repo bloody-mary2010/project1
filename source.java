5dfa3dcce00d015ec6c8f0625094e5db2ef92748
bloody-mary2010 <tvesavatsi@gmail.com>
2024-10-19 09:19:46 +0530

    tvesa

---
+++ source.java
@@ -0,0 +1,10 @@
+import java.util.*; 
+class prog
+{
+    public static void main()
+    {
+        Scanner ob=newScanner(System.in);
+        int a= Math.pow(2,2)
+        System.out.println(a);
+    }
+}
